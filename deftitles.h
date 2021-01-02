05b08222b3c4e1862d729196bcacc555d46621d1
version simple 0.0 <76870235+donoslabastida@users.noreply.github.com>
2021-01-02 07:13:58 -0600

    Create deftitles.h

---
+++ deftitles.h
@@ -0,0 +1,48 @@
+/*
+*------------------------------------------------
+* deftitles.h
+*------------------------------------------------
+*/
+
+#define Label_S_ERROR 		"S_ERROR"
+#define Label_NO_ASSIGNED 	"NO_ASSIGNED"
+#define Label_NO_AVAILABLE  "NO_AVAILABLE"
+#define Label_NO_LINKED 	"NO_LINKED"
+#define Label_OFF 			"OFF"
+#define Label_CLOSED 		"CLOSED"
+#define Label_UNRELEASED	"UN_RELEASED"
+
+#define Label_VACIO 		"VACIO" 
+
+#define Label_ONLINE 		"ONLINE"
+#define Label_REDY 			"READY"
+#define Label_ASSIGNED 		"ASSIGNED"
+#define Label_LINKED 		"LINKED"
+#define Label_AVAILABLE 	"AVAILABLE"
+#define Label_OPEN 			"OPEN"
+
+#define TITLE_APP 			"GRAFICACION DE MAPAS DE RED"
+#define TITLE_FNC_01 		"METRICA DE ARCHIVO"
+#define TITLE_FNC_02 		"METRICA DE VECTOR DE NODOS"
+#define TITLE_FNC_03		"VOLCADO DE GEOMETRIA GRAFICA DE NODOS"
+#define TITLE_FNC_04		"VOLCADO DE GEOMETRIA GRAFICA DE ARISTAS"
+
+#define TITLE_TBL_01 		"TABLA DE CONTENIDO DE VECTOR NODOS"
+#define TITLE_TBL_02		"TABLA DE CONTENIDO DE VECTOR ARISTAS"
+
+#define TITLE_TBL_03 		"TABLA DE METRICA DE ARCHIVO"
+
+#define TITLE_TBL_04 		"TABLA DE BUFFER DE VECTOR NODO X"
+#define TITLE_TBL_05 		"TABLA DE BUFFER DE VECTOR NODO Y"
+#define TITLE_TBL_06 		"TABLA DE BUFFER DE VECTOR ARISTA A"
+#define TITLE_TBL_07 		"TABLA DE BUFFER DE VECTOR ARISTA B"
+
+#define TITLE_TBL_08 		"TABLA DE CONFIGURACION"
+
+#define TITLE_TABLE_1 		" i       Address             Valor           Address             Valor"	
+#define TITLE_TABLE_2 		"                            nodo A                               nodo B"
+#define TITLE_TABLE_3 		"                            Coord X                             Coord Y"
+
+#define TITLE_DIVLINEXTR	"--------------------------------------------------------------------------"
+#define TITLE_DIVLINELNG	"--------------------------------------------------------"
+#define TITLE_DIVLINESRT	"--------------------------------"
