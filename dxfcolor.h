2331a24e8f7dede66c647ebe2d195c9b193c34f1
version simple 0.0 <76870235+donoslabastida@users.noreply.github.com>
2021-01-02 07:14:45 -0600

    Create dxfcolor.h

---
+++ dxfcolor.h
@@ -0,0 +1,13 @@
+// dxfcolor.h
+
+//AUTOCAD colores
+
+#define dxfColorRED			 1
+#define dxfColorYELLOW		 2 
+#define dxfColorGREEN		 3 
+#define dxfColorCYAN		 4
+#define dxfColorBLUE		 5
+#define dxfColorMAGENTA		 6
+#define dxfColorWHITE		 7
+#define dxfColorGRAYDARK	 8 
+#define dxfColorREDGRAYLIGTH 9 
