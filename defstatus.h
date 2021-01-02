640c6e658a73a2b0c486c052ee67961a304fd428
version simple 0.0 <76870235+donoslabastida@users.noreply.github.com>
2021-01-02 07:13:05 -0600

    Create defstatus.h

---
+++ defstatus.h
@@ -0,0 +1,31 @@
+/*
+*------------------------------------------------
+* defstatus.h
+*------------------------------------------------
+*/
+#define FALSE 			0
+#define TRUE 			1
+
+#define S_ERROR 		-1
+#define NO_ASSIGNED 	-1
+#define NO_AVALAIBLE 	-1
+#define NO_LINKED 	    -1
+#define OFF 			-1
+#define CLOSED 			-1
+#define NOT_OPEN		-1
+#define UNRELEASED		-1
+#define UNSELECT 		-1
+#define UNCOMPLETE 		-1
+#define NO_CREATED 		-1
+
+#define VACIO 			0
+
+#define SELECT 			1
+#define ONLINE 			1
+#define READY 			1
+#define ASSIGNED 		1
+#define VINCULATED 		1
+#define AVAILABLE 		1
+#define OPEN 			1
+#define LOADED 			1
+#define CREATED 		1
